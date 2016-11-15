797ca34a4ef3dbda7b9714dbb3bd7131f0da6443
Edward Bowden <ed@edwardbowden.uk>
2016-11-15 11:55:29 +0000

    Finish removing font awesome

--- src/a/upload.php
+++ src/a/upload.php
@@ -36,7 +36,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<script src="../lib/semantic-ui/semantic.min.js" type="text/javascript"></script>
 		<link rel="stylesheet" type="text/css" href="../lib/semantic-ui/semantic.min.css">
-		<link rel="stylesheet" type="text/css" href="../lib/font-awesome/css/font-awesome.min.css">
 	</head>
 	<body>
 
