d8b8abe0b5f5c7165e04b9be9371b5e8c0ef2efd
Edward Bowden <ed@edwardbowden.uk>
2016-11-16 12:09:10 +0000

    Add responsive meta tag

--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@
 <html>
 	<head>
 		<title><?php echo $conf['siteName']; ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<script src="lib/semantic-ui/semantic.min.js" type="text/javascript"></script>
 		<link rel="stylesheet" type="text/css" href="lib/semantic-ui/semantic.min.css">
